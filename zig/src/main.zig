c466eaefee2bb09bd19742599f45e39603731d41
gvdbroek <141612818+gvdbroek@users.noreply.github.com>
2024-07-25 21:08:10 +0200

    got a row to print

--- zig/src/main.zig
+++ zig/src/main.zig
@@ -1,6 +1,13 @@
 const std = @import("std");
 
-pub fn load_board(filename: []const u8) !void {
+const Invalid = error{Empty};
+
+pub fn get_row(board: []Invalid!u8, idx: usize) []Invalid!u8 {
+    const row: []Invalid!u8 = board[(9 * idx)..((idx + 1) * 9)];
+    return row;
+}
+
+pub fn load_board(filename: []const u8) ![81]Invalid!u8 {
     // const realPath: []u8 = std.fs.cwd().realpath(filename);
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena.deinit();
@@ -13,16 +20,39 @@ pub fn load_board(filename: []const u8) !void {
     defer allocator.free(buffer);
     std.debug.print("Loaded data into memory:\n", .{});
 
-    for (buffer) |char| {
-        if (char == '\n') {
+    var board: [9 * 9]Invalid!u8 = undefined;
+    for (0..(9 * 9), 0..) |_, i| {
+        const ch: u8 = buffer[i];
+        const newline: u8 = '\n';
+        if (ch == newline) {
             continue;
         }
-        std.debug.print("{c}", .{char});
+        board[i] = std.fmt.parseInt(u8, buffer[i .. i + 1], 10) catch Invalid.Empty;
     }
-    std.debug.print("\n", .{});
+
+    // std.debug.print("\n", .{});
+    return board;
+
     // std.debug.print("Checking for file @ {}", .{realPath});
 
     // std.fs.openFileAbsolute(absolute_path: []const u8, flags: File.OpenFlags)
+}
+
+pub fn print_value(value: Invalid!u8) void {
+    // var c: u8 = 0;
+    if (value) |v| {
+        // c = std.fmt.digitToChar(v, 10);
+        std.debug.print("{d}", .{v});
+    } else |_| {
+        // c = '-';
+        std.debug.print("-", .{});
+    }
+}
+pub fn print_series(series: []Invalid!u8) void {
+    for (series) |value| {
+        print_value(value);
+    }
+    std.debug.print("\n", .{});
 }
 
 pub fn main() !void {
@@ -30,5 +60,10 @@ pub fn main() !void {
     //
     const sudoku_file = "../boards/test_grid_1.txt";
 
-    try load_board(sudoku_file);
+    var board = try load_board(sudoku_file);
+    var row = get_row(&board, 0);
+    print_series(row);
+    // std.debug.print("{!s}", .{row});
+    board[0] = 1;
+    row[0] = 1;
 }
